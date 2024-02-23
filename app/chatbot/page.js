d0afe061fd375325e077fce979053ae9a240e4d9
krishnan74 <divyakrishnan.r74@gmail.com>
2024-02-23 10:05:06 +0530

    Static user input JSON chatbot integration over

--- app/chatbot/page.js
+++ app/chatbot/page.js
@@ -1,23 +1,13 @@
 "use client";
 import React, { useState } from "react";
+import details from "./details.json"
 
 const ChatPage = () => {
-  const [formData, setFormData] = useState({
-    prompt: "",
-  });
-
-  const [chatHistory, setChatHistory] = useState([]);
-  const [loaderDiv, toggleLoaderDiv] = useState(false);
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
+  
 
   const handleSubmit = async (e) => {
+   
     e.preventDefault();
-    setChatHistory([...chatHistory, { text: formData.prompt, user: true }]);
-    setFormData({ prompt: "" });
 
     try {
       // Prepare the data to be sent to the server
@@ -26,12 +16,12 @@ const ChatPage = () => {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(formData),
+        body: JSON.stringify(details),
       };
 
       // Make a POST request to the Flask server
       const response = await fetch(
-        "http://localhost:8080/chatbot",  // Adjust the URL if your Flask server runs on a different port or host
+        "http://localhost:8080/chatbot",
         requestData
       );
 
@@ -41,67 +31,33 @@ const ChatPage = () => {
 
       const data = await response.json();
 
-      // Update chat history with the bot's response
-      setChatHistory([...chatHistory, { text: data.text, user: false }]);
-      toggleLoaderDiv(false);
+      console.log(data);
+    
     } catch (error) {
       console.error("Error sending data to server:", error.message);
-      toggleLoaderDiv(false);
     }
   };
 
   return (
     <div className="px-[100px]">
-      <div className="flex flex-col h-[400px] border rounded-lg border-black mb-10 shadow-xl overflow-y-auto">
-        {chatHistory.map((message, index) => (
-          <div
-            key={index}
-            className={`${
-              message.user ? "text-right" : "text-left"
-            } mx-4 my-2`}
-          >
-            <span
-              className={`${
-                message.user ? "bg-blue-500" : "bg-gray-300"
-              } text-white px-4 py-2 rounded-lg inline-block`}
-            >
-              {message.text}
-            </span>
-          </div>
-        ))}
-      </div>
-
       <form method="post" onSubmit={handleSubmit}>
-        <div className="flex justify-center gap-5 flex-col">
-          <div className="flex justify-center">
-            <div className="mb-4 w-[800px]">
-              <input
-                type="text"
-                placeholder="Type your message..."
-                id="prompt"
-                name="prompt"
-                value={formData.prompt}
-                onChange={handleChange}
-                className="mt-1 p-2 border rounded-md w-full shadow-md px-4"
-                required
-              ></input>
-            </div>
-          </div>
+        <div className="flex justify-center gap-5 h-[300px] flex-col ">
+          
 
           <div className="flex justify-center">
             <button
               type="submit"
               className="bg-[#4F86E7] text-white font-semibold py-2 px-4 rounded-full shadow-lg"
-              onClick={() => toggleLoaderDiv(true)}
             >
               Submit
             </button>
           </div>
         </div>
       </form>
+
+     
     </div>
   );
 };
 
 export default ChatPage;
-
--- server/chat.py
+++ server/chat.py
@@ -1,26 +1,87 @@
+# from langchain.chat_models import ChatOpenAI
+# from langchain.agents import create_json_agent
+# from langchain.agents.agent_toolkits import JsonToolkit
+# from langchain.tools.json.tool import JsonSpec
+# from flask import Flask, request, jsonify
+# from flask_cors import CORS
+
+# OPENAI_API_KEY = "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"
+
+# app = Flask(__name__)
+# CORS(app, resources={r"/chatbot": {"origins": "http://localhost:3000"}})
+
+# @app.route('/chatbot', methods=['POST'])
+# def process_data():
+#     data = request.get_json()
+#     spec = JsonSpec(dict_=data, max_value_length=4000)
+#     toolkit = JsonToolkit(spec=spec)
+
+#     agent = create_json_agent(llm=ChatOpenAI(openai_api_key=OPENAI_API_KEY, temperature=0, model="gpt-3.5-turbo"), toolkit=toolkit, max_iterations=1000, verbose=True)
+
+#     user_input = data.get('user_input', '')  # Assuming the JSON contains a 'user_input' key
+
+#     response = agent.run(user_input)
+
+#     return jsonify({'response': response})
+
+# if __name__ == '__main__':
+#     app.run(debug=True, port=8080)
+
+from flask import Flask, request, jsonify, send_file
+from flask_cors import CORS
+import pandas as pd 
+from sklearn.ensemble import RandomForestClassifier 
+import matplotlib.pyplot as plt
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+from sklearn.preprocessing import LabelEncoder 
+from reportlab.lib.pagesizes import letter 
+from reportlab.platypus import SimpleDocTemplate, Paragraph 
+from reportlab.lib.styles import getSampleStyleSheet 
+from reportlab.lib import colors 
+from langchain_community.utilities import WikipediaAPIWrapper
+from reportlab.lib.units import inch
+from io import BytesIO
 from langchain.chat_models import ChatOpenAI
 from langchain.agents import create_json_agent
 from langchain.agents.agent_toolkits import JsonToolkit
 from langchain.tools.json.tool import JsonSpec
-from flask import Flask, request, jsonify
+import os
 
-OPENAI_API_KEY = "api_key"
 
 app = Flask(__name__)
 
-@app.route('/patient/<string:patient_id>', methods=['POST'])
-def process_data(patient_id):
-    data = request.get_json()
-    spec = JsonSpec(dict_=data, max_value_length=4000)
-    toolkit = JsonToolkit(spec=spec)
 
-    agent = create_json_agent(llm=ChatOpenAI(openai_api_key=OPENAI_API_KEY, temperature=0, model="gpt-3.5-turbo"), toolkit=toolkit, max_iterations=1000, verbose=True)
+CORS(app, resources={r"/chatbot": {"origins": "http://localhost:3000"}})
 
-    user_input = data.get('user_input', '')  # Assuming the JSON contains a 'user_input' key
 
-    response = agent.run(user_input)
 
-    return jsonify({'patient_id': patient_id, 'response': response})
+@app.route("/chatbot", methods=["POST"])
+def chatbot():
+    OPENAI_API_KEY = "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"
+    if request.method == "POST":
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+        # Get the JSON data sent from the client
+        data = request.get_json()
+
+        spec = JsonSpec(dict_=data, max_value_length=4000)
+        toolkit = JsonToolkit(spec=spec)
+
+        agent = create_json_agent(llm=ChatOpenAI(openai_api_key=OPENAI_API_KEY, temperature=0, model="gpt-3.5-turbo"), toolkit=toolkit, max_iterations=1000, verbose=True)
+
+        
+        response = agent.run("how many patients are there ?")
+
+        return jsonify(response)
+    
+    
+    else:
+        # This is a GET request, return a message indicating it's not supported
+        return jsonify({
+            "message": "GET request not supported for this route"
+        })
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8080)

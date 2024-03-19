440102363950c1d173ab7b91b7b2692c1a662fc9
JadeSamLee <113660177+JadeSamLee@users.noreply.github.com>
2024-03-19 22:13:13 +0530

    Update page.js

--- app/chatbot/page.js
+++ app/chatbot/page.js
@@ -1,13 +1,16 @@
 "use client";
 import React, { useState } from "react";
-import details from "./details.json"
+import details from "./details.json";
 
 const ChatPage = () => {
   
+  const [prompt, setPrompt] = useState("");
+  const [reply, setReply] = useState("");
+  const [submitted , setSubmittes] = useState(false);
 
   const handleSubmit = async (e) => {
-   
     e.preventDefault();
+    
 
     try {
       // Prepare the data to be sent to the server
@@ -16,7 +19,8 @@ const ChatPage = () => {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(details),
+        body: JSON.stringify({data : details,
+          userInput: prompt}),
       };
 
       // Make a POST request to the Flask server
@@ -30,9 +34,11 @@ const ChatPage = () => {
       }
 
       const data = await response.json();
+      setReply(data);
 
-      console.log(data);
-    
+      // Update chat history with the bot's response
+      
+      
     } catch (error) {
       console.error("Error sending data to server:", error.message);
     }
@@ -41,12 +47,19 @@ const ChatPage = () => {
   return (
     <div className="px-[100px]">
       <form method="post" onSubmit={handleSubmit}>
-        <div className="flex justify-center gap-5 h-[300px] flex-col ">
-          
+        <div className="flex justify-center gap-5 flex-col">
+          <div>
+            {reply}
+          </div>
+
+
+          <input type="text" className="py-3 px-5" value={prompt} onChange={(e) => setPrompt(e.target.value)}/>
+
 
           <div className="flex justify-center">
             <button
               type="submit"
+              
               className="bg-[#4F86E7] text-white font-semibold py-2 px-4 rounded-full shadow-lg"
             >
               Submit
